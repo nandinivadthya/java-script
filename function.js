ed715140e0e9f1b6029ea7c3ba9b5b8586082dbd
Nandhini-213 <nandinivadthya08@gmail.com>
2025-09-22 14:21:42 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+// functions in js is used to perform a multiple tasks in a single time
+function add(){
+    var a=10
+    var b=20
+    document.write(a+b);
+    console.log(a+b);
+    return a+b
+}
+let name="siri";
+
+function demo(){
+    let name="siri";
+    document.write('hello,my name is $(name)');
+    demo();
+}
