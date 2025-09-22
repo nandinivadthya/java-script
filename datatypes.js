bb696528ac92a1a59361a819b20bc9b21bfb92ca
Nandhini-213 <nandinivadthya08@gmail.com>
2025-09-22 14:23:57 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script >
+        var_a=67
+        console.log(var_a);
+        var b=89
+        console.log(b);
+        // data types in js
+        // primitive data types
+        // 1. number, 2. string, 3. boolean, 4. null, 5. undefined
+        // non primitive data types
+        // 1. object, 2. array, 3. function
+        // this and window are commands of js
+        // datatypes in js
+var a=67
+console.log(a);
+var b="hello"  //string
+console.log(b);
+var c=true  //boolean
+console.log(c);
+var d;  //undefined
+console.log(d);
+var e=null  //null  
+console.log(e);
+//non primitive datatypes
+Array=[67,"anu",true,null];
+console.log(typeof Array)
+
+     obj ={
+        name ="anu"
+        age ="21"
+        dept ="aiml"
+     }
+     console.log(typeof obj)
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
